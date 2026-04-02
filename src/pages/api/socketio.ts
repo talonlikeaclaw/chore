562780035b196a95b79e1b05f1f206e7cf6fcdd4
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-04-02 20:18:25 +0000

    Add pages/api/socketio.ts with socket handler/types

---
+++ src/pages/api/socketio.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+import type { Server as HTTPServer } from "http";
+import type { Socket as NetSocket } from "net";
+
+interface SocketServer extends HTTPServer {
+  io?: SocketIOServer;
+}
+
+interface SocketWithServer extends NetSocket {
+  server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithServer;
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponseWithSocket
+) {
+  if (!res.socket.server.io) {
+    const io = new SocketIOServer(res.socket.server, {
+      path: "/api/socketio",
+      addTrailingSlash: false,
+    });
+
+    res.socket.server.io = io;
+
+    io.on("connection", (socket) => {
+      console.log("Socket connected:", socket.id);
+
+      socket.on("ping", () => {
+        socket.emit("pong");
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Socket disconnected:", socket.id);
+      });
+    });
+  }
+
+  res.end();
+}
