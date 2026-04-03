2bbf8c91582b8daba2a61fb4a7f1484db6a7bac1
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-04-03 16:02:20 -0400

    Fix postgres host issue in postgres URL with POSTGRES_HOST

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,9 +5,10 @@ const globalForDb = globalThis as unknown as {
   db: ReturnType<typeof drizzle<typeof schema>> | undefined;
 };
 
+const host = process.env.POSTGRES_HOST ?? "localhost";
 const url =
   process.env.DATABASE_URL ??
-  `postgresql://${encodeURIComponent(process.env.POSTGRES_USER!)}:${encodeURIComponent(process.env.POSTGRES_PASSWORD!)}@localhost:5432/${process.env.POSTGRES_DB}`;
+  `postgresql://${encodeURIComponent(process.env.POSTGRES_USER!)}:${encodeURIComponent(process.env.POSTGRES_PASSWORD!)}@${host}:5432/${process.env.POSTGRES_DB}`;
 
 export const db = globalForDb.db ?? drizzle(url, { schema });
 
